fb5789d7add42c957311b495a528dac4f8d50d50
Ursulina Kölbener <ursulina@koelbener.ch>
2022-11-04 16:01:10 +0100

    adding documentation to ft_to_cm function

--- R/ft_to_cm.R
+++ R/ft_to_cm.R
@@ -1,3 +1,17 @@
+#' Converting Feet to Centimeters
+#'
+#' In 1959 the international yard and pound agreement (between the United
+#' States and countries of the Commonwealth of Nations) defined a yard as
+#' being exactly 0.9144 metres, which in turn defined the foot as being
+#' exactly 0.3048 metres (304.8 mm).
+#'
+#' @param feet (numeric)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' ft_to_cm(3)
 ft_to_cm <- function(feet) {
   cm <- feet / 0.032808
   return(cm)
